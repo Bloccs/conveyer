288aad7ebd882385938f75e75a6471a25bcd0520
Allan MacGregor <amacgregor@allanmacgregor.com>
2019-02-10 18:53:28 -0500

    test: update test suite to include other block types

--- test/integration/definitions_test.exs
+++ test/integration/definitions_test.exs
@@ -11,4 +11,11 @@ defmodule ConveyerTest.DefinitionsTest do
     assert(Code.ensure_compiled?(Conveyer.Types.PhaseBlock) === true)
   end
 
+  test "there is a base check block defined" do
+    assert(Code.ensure_compiled?(Conveyer.Types.CheckBlock) === true)
+  end
+
+  test "there is a base joint block defined" do
+    assert(Code.ensure_compiled?(Conveyer.Types.JointBlock) === true)
+  end
 end
